6dd2874d23c286a216b8d78387837d4bbd484bfb
Muller Lee <s094392@gmail.com>
2021-03-11 22:10:47 +0800

    Go home

--- Makefile
+++ Makefile
@@ -21,5 +21,5 @@ run: all
 	qemu-system-aarch64 -M raspi3 -kernel kernel8.img -serial null -serial stdio
 
 debug: all
-	tilix -a app-new-session -e "qemu-system-aarch64 -M raspi3 -kernel kernel8.img -serial null -serial stdio -s -S" 
+	tilix -a app-new-session -e "qemu-system-aarch64 -M raspi3 -kernel kernel8.img -display none -serial null -serial stdio -s -S" 
 	tilix -a app-new-session -e "./debug.sh"
--- src/io/init.rs
+++ src/io/init.rs
@@ -1,26 +1,27 @@
-use core::{marker::PhantomData, ops};
+use core::{ops};
 use register::{mmio::*, register_bitfields, register_structs};
 
-pub struct MMIODerefWrapper<T> {
-    start_addr: usize,
-    phantom: PhantomData<fn() -> T>,
+pub struct UARTDriver {
+    base_addr: usize,
 }
 
-impl<T> MMIODerefWrapper<T> {
-    /// Create an instance.
-    pub const unsafe fn new(start_addr: usize) -> Self {
-        Self {
-            start_addr,
-            phantom: PhantomData,
-        }
+impl UARTDriver {
+
+    fn ptr(&self) -> *const RegisterBlock {
+        self.base_addr as *const _
+    }
+
+    fn init(&self) {
+        self.AUXENB.write(AUXENB::MINIUART::Disable);
+        self.GPFSEL1.write(GPFSEL1::FSEL14::ALT5);
     }
 }
 
-impl<T> ops::Deref for MMIODerefWrapper<T> {
-    type Target = T;
+impl ops::Deref for UARTDriver {
+    type Target = RegisterBlock;
 
     fn deref(&self) -> &Self::Target {
-        unsafe { &*(self.start_addr as *const _) }
+        unsafe { &*self.ptr() }
     }
 }
 
@@ -38,32 +39,27 @@ register_bitfields! {
             Output = 0b001,
             ALT5 = 0b010
         ]
+    ],
+
+    AUXENB [
+        MINIUART OFFSET(0) NUMBITS(1) [
+            Enable = 0b1,
+            Disable = 0b0
+        ]
     ]
-    //AUXENB [
-        //MINIUART OFFSET(0) NUMBITS(1) [
-            //ENABLE = 0b1,
-            //DISABLE = 0b0,
-        //]
-    //]
 }
 
 register_structs! {
     #[allow(non_snake_case)]
     pub RegisterBlock {
         (0x00200004 => GPFSEL1: ReadWrite<u32, GPFSEL1::Register>),
-        //(0x00215004 => AUXENB: ReadWrite<u32, GPFSEL1::Register>),
-        (0x00300000 => @END),
+        (0x00215004 => AUXENB: ReadWrite<u32, AUXENB::Register>),
+        (0x01000000 => @END),
     }
 }
 
-type Registers = MMIODerefWrapper<RegisterBlock>;
 
 pub fn uart_init() {
-    unsafe {
-        let regs = Registers::new(0x3F000000);
-        regs.GPFSEL1.write(GPFSEL1::FSEL14::ALT5);
-        //regs.GPFSEL1.write(GPFSEL1::FSEL15::ALT5);
-        regs.GPFSEL1.set(0x3);
-        //regs.AUXENB.write(AUXENB::MINIUART::ENABLE);
-    }
+    let uart_driver = UARTDriver{base_addr: 0x3F000000};
+    uart_driver.init();
 }
